0d3f5e19d6948b2760f7f7bdd16ab50fd7b3328a
kashaf shoukat <139368562+kashaffshoukat@users.noreply.github.com>
2025-03-03 21:23:26 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  experimental: {
-    turbo: false, // ❌ Disable Turbopack
-  },
-};
-
-module.exports = nextConfig;
+  /* config options here */
+}
+ 
+module.exports = nextConfig
